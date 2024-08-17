cc2475de5d9277abe70e50d92f54c8d9658184a4
Stefan Sabev <stevenbg91@gmail.com>
2024-08-17 09:33:38 +0200

    Add Routes for the pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,12 @@
-import Navbar from './components/navbar/Navbar'
+import Navbar from './components/navbar/Navbar';
+import Hero from './components/hero/Hero';
+import LatestBlog from './components/latestBlog/LatestBlog';
+import LatestNews from './components/latestNews/LatestNews';
+import Footer from './components/footer/Footer';
+import { Route, Router, Routes } from 'react-router';
+import Main from './components/main/Main';
+import World from './components/world/World';
+import Article from './components/article/MainArticle';
 
 
 function App() {
@@ -6,7 +14,17 @@ function App() {
 
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/blog" element={<LatestBlog />} />
+        <Route path="/world" element={<Article name={"World News"} />} />
+        <Route path="/tech" element={<Article name={"Tech News"}/>} />
+        <Route path="/sport" element={<Article name={"Sport News"}/>} />
+        <Route path="/culture" element={<Article name={"Culture News"}/>} />
+
+      </Routes>
+      <Footer />
     </>
   )
 }

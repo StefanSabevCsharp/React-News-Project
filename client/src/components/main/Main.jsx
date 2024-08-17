67c4137e546ddb8481e66da2e7efd20b8cdad3c1
Stefan Sabev <stevenbg91@gmail.com>
2024-08-17 09:33:14 +0200

    Add Main component for the home screen

---
+++ client/src/components/main/Main.jsx
@@ -0,0 +1,14 @@
+import Hero from "../hero/Hero";
+import LatestBlogs from "../latestBlog/LatestBlog";
+import LatestNews from "../latestNews/LatestNews";
+
+
+export default function Main() {
+  return (
+    <>
+    <Hero />
+    <LatestBlogs />
+    <LatestNews />
+    </>
+  );
+}

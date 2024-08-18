21cb5fb82e57e915333df50236598ef914eb7f9d
Stefan Sabev <stevenbg91@gmail.com>
2024-08-18 07:51:24 +0200

    Refactored Main article, Add cleaning duplicated articles

--- client/src/components/article/MainArticle.jsx
+++ client/src/components/article/MainArticle.jsx
@@ -1,42 +1,30 @@
+import { useState } from "react";
 import AllNewsSection from "./allNewsSection/AllNewsSection";
 import FeaturedArticles from "./featuredArticles/FeaturedArticles";
+import { useGetNews } from "../../hooks/useGetNews";
 
 
 export default function Article({name}) {
-  const mainArticle = {
-    title: 'Big News Headline',
-    image: '/test.webp',
-    link: '/news/big-news-headline',
-  };
 
-  const smallArticles = [
-    {
-      title: 'Small News 1',
-      image: 'mini.jpg',
-      description: 'Brief description of the first small news article.',
-      link: '/news/small-news-1',
-      date: 'June 1,2024',
-    },
-    {
-      title: 'Small News 2',
-      image: 'https://source.unsplash.com/400x300/?news,2',
-      description: 'Brief description of the second small news article.',
-      link: '/news/small-news-2',
-      date: 'June 1,2024',
-    },
-    {
-      title: 'Small News 3',
-      image: 'https://source.unsplash.com/400x300/?news,3',
-      description: 'Brief description of the third small news article.',
-      link: '/news/small-news-3',
-      date: 'June 1,2024',
-    },
-  ];
+  const [news,setNews] = useGetNews(name);
+  console.log(news);
 
+  function cleanDuplicateNews(news){
+    const uniqueNews = news.filter((news, index, self) =>
+    index === self.findIndex((t) => (
+      t.title === news.title || t.urlToImage === news.urlToImage
+      
+    ))
+  )
+  return uniqueNews;
+  }
+  const newsToSend = cleanDuplicateNews(news);
+
+ 
   return (
     <>
-    <FeaturedArticles mainArticle={mainArticle} smallArticles={smallArticles} />
-    <AllNewsSection category={name}/>
+    <FeaturedArticles feauturedNews={newsToSend.slice(0.6)} />
+    <AllNewsSection category={name} news={newsToSend.slice(7)}/>
     </>
   );
 }

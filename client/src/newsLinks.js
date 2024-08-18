dd035ee72b5c0ced494f62dcf468935941d25e4b
Stefan Sabev <stevenbg91@gmail.com>
2024-08-18 07:52:46 +0200

    Add list of api links for every different news category

---
+++ client/src/newsLinks.js
@@ -0,0 +1,20 @@
+
+
+ const worldNews = "https://newsapi.org/v2/everything?sources=abc-news&apiKey=79d8492550ec4703a33b4a8bbc5a3d3b";
+
+const entertainmentNews = "https://newsapi.org/v2/everything?sources=techcrunch&apiKey=79d8492550ec4703a33b4a8bbc5a3d3b";
+
+const businessNews = "https://newsapi.org/v2/everything?sources=bloomberg&apiKey=79d8492550ec4703a33b4a8bbc5a3d3b";
+
+
+const sportNews = "https://newsapi.org/v2/everything?sources=bbc-sport&apiKey=79d8492550ec4703a33b4a8bbc5a3d3b";
+
+ const techNews = "https://newsapi.org/v2/everything?sources=ars-technica&apiKey=79d8492550ec4703a33b4a8bbc5a3d3b";
+
+export const apiLink ={
+    worldNews,
+    entertainmentNews,
+    businessNews,
+    sportNews,
+    techNews
+}

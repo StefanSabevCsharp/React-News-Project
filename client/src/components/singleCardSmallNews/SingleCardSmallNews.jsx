9290d5eeb28d98fcd9a62dfda800e65b1dddb09f
Stefan Sabev <stevenbg91@gmail.com>
2024-08-21 21:13:11 +0200

    Update SingleCardSmallNews.jsx

--- client/src/components/singleCardSmallNews/SingleCardSmallNews.jsx
+++ client/src/components/singleCardSmallNews/SingleCardSmallNews.jsx
@@ -1,18 +1,20 @@
-export default function SingleCardSmallNews() {
+import { dateParser } from "../../utils/dateParser";
+
+export default function SingleCardSmallNews({ article }) {
     return (
         <a
-            href="/world-news-1"
+            href={article?.url}
             className="relative rounded-lg border border-gray-300 bg-white shadow-md overflow-hidden transition-transform duration-300 hover:scale-105 hover:bg-gray-100"
         >
             <img
-                src="https://source.unsplash.com/800x600/?world,news"
+                src={article.urlToImage}
                 alt="World News 1"
                 className="w-full h-48 object-cover object-center"
             />
             <div className="p-4 flex flex-col h-full">
-                <h3 className="text-xl font-semibold mb-1">World News Title 1</h3>
+                <h3 className="text-xl font-semibold mb-1">{article.description}</h3>
                 <p className="text-gray-500 text-sm border-b border-gray-300 pb-2">
-                    January 1, 2024
+                {article.publishedAt ? dateParser(article.publishedAt) : "Unknown Date"} 
                 </p>
             </div>
         </a>

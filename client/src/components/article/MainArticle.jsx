4f2b79af36db65b4e825cfbc1e4becc648cbf1bb
Stefan Sabev <stevenbg91@gmail.com>
2024-08-22 21:10:52 +0200

    Update MainArticle.jsx

--- client/src/components/article/MainArticle.jsx
+++ client/src/components/article/MainArticle.jsx
@@ -1,30 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AllNewsSection from "./allNewsSection/AllNewsSection";
 import FeaturedArticles from "./featuredArticles/FeaturedArticles";
 import { useGetNews } from "../../hooks/useGetNews";
 
 
-export default function Article({name}) {
+export default function Article({ name }) {
 
-  const [news,setNews] = useGetNews(name);
+  const [news, setNews] = useGetNews(name);
+  const [loading, setLoading] = useState(true);
   console.log(news);
 
-  function cleanDuplicateNews(news){
+  function cleanDuplicateNews(news) {
     const uniqueNews = news.filter((news, index, self) =>
-    index === self.findIndex((t) => (
-      t.title === news.title || t.urlToImage === news.urlToImage
-      
-    ))
-  )
-  return uniqueNews;
+      index === self.findIndex((t) => (
+        t.title === news.title || t.urlToImage === news.urlToImage
+
+      ))
+    )
+    return uniqueNews;
   }
   const newsToSend = cleanDuplicateNews(news);
 
- 
+  useEffect(() => {
+    if (newsToSend.length > 0) {
+      setLoading(false);
+    }
+  }, [newsToSend])
+
+
   return (
     <>
-    <FeaturedArticles feauturedNews={newsToSend.slice(0.6)} />
-    <AllNewsSection category={name} news={newsToSend.slice(7)}/>
+    {!loading &&  <FeaturedArticles feauturedNews={newsToSend.slice(0.6)} /> }
+    {!loading && <AllNewsSection category={name} news={newsToSend.slice(7)} />} 
     </>
   );
 }
